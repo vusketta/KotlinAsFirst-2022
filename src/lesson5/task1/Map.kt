5e76c250e7d18b1e728132421e18ed7b742f0f4a
Vusketta <vusketta@gmail.com>
2022-09-16 23:09:27 +0300

    canBuildFrom fixed

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -203,7 +203,8 @@ fun findCheapestStuff(stuff: Map<String, Pair<String, Double>>, kind: String): S
  * Например:
  *   canBuildFrom(listOf('a', 'b', 'o'), "baobab") -> true
  */
-fun canBuildFrom(chars: List<Char>, word: String): Boolean = chars.containsAll(word.toSet())
+fun canBuildFrom(chars: List<Char>, word: String): Boolean =
+    chars.map { it.lowercaseChar() }.containsAll(word.lowercase().toSet())
 
 /**
  * Средняя (4 балла)
